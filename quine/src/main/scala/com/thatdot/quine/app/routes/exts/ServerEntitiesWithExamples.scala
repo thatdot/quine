521e300253ff48cda09cf5b1e9b04def8f5a78cf
Leif Warner <leif@thatdot.com>
2023-01-12 12:10:52 -0800

    QU-1008: Use JSON parser for JSON request bodies; use explicits rather than implicitsUse explicit references, factor out Akka HTTP boilerplate
    * Explicitly apply the implicit conversion used in akka unmarshaller
    
    This also lets us use the named value instead of writing `implicitly`
    
    * Factored out all the common Akka HTTP Entity / Directive boilerplate into helper
    
    There's a fair amount of wild-looking Akka HTTP boilerplate going on
    between csvRequest and yamlRequest.
    Hopefully these look less inscrutable with the weird Akka HTTP stuff
    factored out into a standalone method, and just the logic describing
    HttpEntity => Future[A] (how to parse the request body) left behind.
    Also hopefully saves us from e.g. forgetting to add `forContentType` on
    an addition / refactor.
    
    * Turn csvRequest into a lazy val - it doesn't take params.
    
    But also it can't be `val`, because it depends on stuff in other traits
    yet to be instantiated at the time this is instantiated.
    
    Also remove unnecessary `implicit`
    
    GitOrigin-RevId: 58a50a520da971a5917974680931961cec344906

--- quine/src/main/scala/com/thatdot/quine/app/routes/exts/ServerEntitiesWithExamples.scala
+++ quine/src/main/scala/com/thatdot/quine/app/routes/exts/ServerEntitiesWithExamples.scala
@@ -4,8 +4,8 @@ import java.io.InputStream
 
 import scala.concurrent.Future
 
-import akka.http.scaladsl.model.{HttpEntity, MediaTypes, StatusCodes}
-import akka.http.scaladsl.server.Directives
+import akka.http.scaladsl.model.{ContentTypeRange, HttpEntity, MediaTypes, StatusCodes}
+import akka.http.scaladsl.server.{Directive1, Directives}
 import akka.http.scaladsl.unmarshalling._
 import akka.stream.Materializer
 import akka.stream.alpakka.csv.scaladsl.CsvParsing
@@ -24,56 +24,45 @@ trait ServerEntitiesWithExamples
     with JsonEntitiesFromSchemas
     with endpoints4s.akkahttp.server.EndpointsWithCustomErrors {
 
-  def csvRequest: RequestEntity[List[List[String]]] = {
-    val csvUnmarshaller: FromRequestUnmarshaller[List[List[String]]] = {
-      implicit val entityUnmarshaller: FromEntityUnmarshaller[List[List[String]]] =
-        Unmarshaller
-          .withMaterializer { _ => implicit mat => (entity: HttpEntity) =>
-            val charset = Unmarshaller.bestUnmarshallingCharsetFor(entity).nioCharset
-            entity.dataBytes
-              .via(CsvParsing.lineScanner())
-              .map(_.view.map(_.decodeString(charset)).toList)
-              .named("csv-unmarshaller")
-              .runWith(Sink.collection[List[String], List[List[String]]])
-          }
-          .forContentTypes(MediaTypes.`text/csv`)
-      implicitly
+  /** Helper function for turning an HTTP request body => A function into an Akka Unmarshaller / Directive */
+  protected def unmarshallerFor[A](
+    contentType: ContentTypeRange
+  )(
+    f: (Materializer, HttpEntity) => Future[A]
+  ): Directive1[A] = Directives.entity[A](
+    Unmarshaller.messageUnmarshallerFromEntityUnmarshaller(
+      Unmarshaller.withMaterializer[HttpEntity, A](_ => mat => entity => f(mat, entity)).forContentTypes(contentType)
+    )
+  )
+  lazy val csvRequest: RequestEntity[List[List[String]]] =
+    unmarshallerFor(MediaTypes.`text/csv`) { (mat, entity) =>
+      val charset = Unmarshaller.bestUnmarshallingCharsetFor(entity).nioCharset
+      entity.dataBytes
+        .via(CsvParsing.lineScanner())
+        .map(_.view.map(_.decodeString(charset)).toList)
+        .named("csv-unmarshaller")
+        .runWith(Sink.collection[List[String], List[List[String]]])(mat)
     }
 
-    Directives
-      .entity[List[List[String]]](csvUnmarshaller)
-  }
-
-  private def requestEntityAsInputStream(entity: HttpEntity)(implicit materializer: Materializer): InputStream =
+  private def requestEntityAsInputStream(entity: HttpEntity)(materializer: Materializer): InputStream =
     entity.dataBytes.runWith(StreamConverters.asInputStream())(materializer)
 
-  def yamlRequest[A](implicit schema: JsonSchema[A]): RequestEntity[A] = {
-    val yamlUnmarshaller: FromRequestUnmarshaller[Validated[A]] = {
-      implicit val entityUnmarshaller: FromEntityUnmarshaller[Validated[A]] =
-        Unmarshaller
-          .withMaterializer(_ =>
-            implicit mat =>
-              (entity: HttpEntity) =>
-                Future {
-                  // While the conversion from Akka Stream Source to a java.io.InputStream
-                  // does not block, the subsequent use of the InputStream (yaml.parseToJson)
-                  // does involve blocking "io", hence that is done on a blocking thread.
-                  // "the users of the materialized value, InputStream, [...] will block" - akka/akka#30831
-                  val requestInputStream = requestEntityAsInputStream(entity)
-                  schema.decoder.decode(
-                    yaml.parseToJson(requestInputStream)
-                  )
-                }(new QuineDispatchers(mat.system).blockingDispatcherEC)
-          )
-          .forContentTypes(QuineMediaTypes.`application/yaml`, MediaTypes.`application/json`)
-      implicitly
-    }
-
-    Directives.entity[Validated[A]](yamlUnmarshaller).flatMap {
+  def yamlRequest[A](implicit schema: JsonSchema[A]): RequestEntity[A] =
+    unmarshallerFor[Validated[A]](QuineMediaTypes.`application/yaml`)((mat, entity) =>
+      Future {
+        // While the conversion from Akka Stream Source to a java.io.InputStream
+        // does not block, the subsequent use of the InputStream (yaml.parseToJson)
+        // does involve blocking "io", hence that is done on a blocking thread.
+        // "the users of the materialized value, InputStream, [...] will block" - akka/akka#30831
+        val requestInputStream = requestEntityAsInputStream(entity)(mat)
+        schema.decoder.decode(
+          yaml.parseToJson(requestInputStream)
+        )
+      }(new QuineDispatchers(mat.system).blockingDispatcherEC)
+    ).flatMap {
       case Valid(a) => Directives.provide(a)
       case inv: Invalid => handleClientErrors(inv)
     }
-  }
 
   val ServiceUnavailable = StatusCodes.ServiceUnavailable
 }
