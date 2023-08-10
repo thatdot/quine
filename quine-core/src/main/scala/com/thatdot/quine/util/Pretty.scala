4ec6737ac6f219d5adb611be4c02fda7ef7e32ef
Ben Kyrlach <bkyrlach@gmail.com>
2023-08-10 11:30:24 -0400

    Added a pretty printer for MVSQ DSL
    * Added a pretty printer for MVSQ DSL
    Added tests to validate some basic outputs.
    
    * `sbt fixall` changes
    
    * Fixes for compatability across scala versions. Specifically...
    
    Introduced standardized formatting for Symbol.
    Introduced standardized formatting for Option[Symbol].
    
    * Final version of the test based on Matthew's suggestion. A new version will be forthcoming with build changes to ignore the tests on 2.13
    
    * Using Christophers suggested fix. Tests now use string interpolation to splice in Symbol values that are expected.
    
    * Removing out partially implemented show instance hierarchy. Leif has suggested that this might be useful in the future, however.
    
    ---------
    
    Co-authored-by: Benjamin Kyrlach <benjaminkyrlach@Benjamins-MacBook-Pro.local>
    GitOrigin-RevId: fc1221ab11f786a9fdd295c76b871247e3e4fcb9

---
+++ quine-core/src/main/scala/com/thatdot/quine/util/Pretty.scala
@@ -0,0 +1,56 @@
+package com.thatdot.quine.util
+
+import com.thatdot.quine.graph.cypher.MultipleValuesStandingQuery
+
+object Pretty {
+
+  def tabString(numTabs: Int): String =
+    (1 to numTabs).map(_ => "\t").mkString
+
+  def treePrint(mvsq: MultipleValuesStandingQuery, indent: Int): String = {
+    val sp = tabString(indent)
+    val sp1 = tabString(indent + 1)
+    mvsq match {
+      case MultipleValuesStandingQuery.UnitSq() => sp + "Unit"
+      case MultipleValuesStandingQuery.Cross(queries, emitSubscriptionsLazily, _) =>
+        val queriesString = s"List(\n${queries.map(q => treePrint(q, indent + 2)).mkString(",\n")}\n$sp1)"
+        s"""${sp}Cross (
+           |${sp1}queries = $queriesString,
+           |${sp1}emitSubscriptionsLazily = $emitSubscriptionsLazily
+           |${sp})""".stripMargin
+      case MultipleValuesStandingQuery.LocalProperty(propKey, propConstraint, aliasedAs, _) =>
+        s"""${sp}LocalProperty (
+           |${sp1}propKey = $propKey,
+           |${sp1}propConstraint = $propConstraint,
+           |${sp1}aliasedAs = $aliasedAs
+           |${sp})""".stripMargin
+      case MultipleValuesStandingQuery.LocalId(aliasedAs, formatAsString, _) =>
+        s"""${sp}LocalId (
+           |${sp1}aliasedAs = $aliasedAs,
+           |${sp1}formatAsString = $formatAsString
+           |${sp})""".stripMargin
+      case MultipleValuesStandingQuery.SubscribeAcrossEdge(edgeName, edgeDirection, andThen, _) =>
+        s"""${sp}SubscribeAcrossEdge (
+           |${sp1}edgeName = $edgeName,
+           |${sp1}edgeDirection = $edgeDirection,
+           |${sp1}andThen =
+           |${treePrint(andThen, indent + 2)}
+           |${sp})""".stripMargin
+      case MultipleValuesStandingQuery.EdgeSubscriptionReciprocal(halfEdge, andThenId, _) =>
+        s"""${sp}EdgeSubscriptionReciprocal (
+           |${sp1}halfEdge = $halfEdge,
+           |${sp1}andThenId = $andThenId
+           |${sp})""".stripMargin
+      case MultipleValuesStandingQuery.FilterMap(condition, toFilter, dropExisting, toAdd, _) =>
+        s"""${sp}FilterMap (
+           |${sp1}condition = $condition,
+           |${sp1}toFilter =
+           |${treePrint(toFilter, indent + 2)},
+           |${sp1}dropExisting = $dropExisting,
+           |${sp1}toAdd = $toAdd
+           |${sp})""".stripMargin
+    }
+  }
+
+  def treePrint(mvsq: MultipleValuesStandingQuery): String = treePrint(mvsq, 0)
+}
---
+++ quine-core/src/test/scala/com/thatdot/quine/util/PrettyTests.scala
@@ -0,0 +1,159 @@
+package com.thatdot.quine.util
+
+import java.util.UUID
+
+import scala.collection.compat.immutable.ArraySeq
+
+import org.scalatest.flatspec.AnyFlatSpec
+import org.scalatestplus.scalacheck.ScalaCheckDrivenPropertyChecks
+
+import com.thatdot.quine.graph.MultipleValuesStandingQueryPartId
+import com.thatdot.quine.graph.cypher.{Expr, MultipleValuesStandingQuery}
+import com.thatdot.quine.model.{EdgeDirection, HalfEdge, QuineId}
+
+class PrettyTests extends AnyFlatSpec with ScalaCheckDrivenPropertyChecks {
+  "tabString" should "generate empty string for negative/zero input" in {
+    assert(Pretty.tabString(-1) == "")
+  }
+
+  "treePrint(UnitSq)" should "generate expected pretty output" in {
+    val result = Pretty.treePrint(MultipleValuesStandingQuery.UnitSq())
+
+    val expected = "Unit"
+
+    assert(result == expected)
+  }
+
+  "treePrint(exampleCross)" should "generate expected pretty output" in {
+    val exampleCross = MultipleValuesStandingQuery.Cross(
+      queries = ArraySeq(
+        List(
+          MultipleValuesStandingQuery.UnitSq(),
+          MultipleValuesStandingQuery.UnitSq()
+        ): _*
+      ),
+      emitSubscriptionsLazily = true
+    )
+
+    val result = Pretty.treePrint(exampleCross)
+
+    val expected =
+      s"""Cross (
+         |\tqueries = List(
+         |\t\tUnit,
+         |\t\tUnit
+         |\t),
+         |\temitSubscriptionsLazily = true
+         |)""".stripMargin
+
+    assert(result == expected)
+  }
+
+  "treePrint(exampleLocalProp)" should "generate expected pretty output" in {
+    val name = Symbol("name")
+    val pname = Some(Symbol("p.name"))
+
+    val exampleLocalProp = MultipleValuesStandingQuery.LocalProperty(
+      propKey = name,
+      propConstraint = MultipleValuesStandingQuery.LocalProperty.Any,
+      aliasedAs = pname
+    )
+
+    val result = Pretty.treePrint(exampleLocalProp)
+
+    val expected =
+      s"""LocalProperty (
+         |\tpropKey = $name,
+         |\tpropConstraint = Any,
+         |\taliasedAs = $pname
+         |)""".stripMargin
+
+    assert(result == expected)
+  }
+
+  "treePrint(exampleLocalId)" should "generate expected pretty output" in {
+    val unknown = Symbol("unknown")
+
+    val exampleLocalId = MultipleValuesStandingQuery.LocalId(
+      aliasedAs = unknown,
+      formatAsString = true
+    )
+
+    val result = Pretty.treePrint(exampleLocalId)
+
+    val expected =
+      s"""LocalId (
+         |\taliasedAs = $unknown,
+         |\tformatAsString = true
+         |)""".stripMargin
+
+    assert(result == expected)
+  }
+
+  "treePrint(exampleEdgeSub)" should "generate expected pretty output" in {
+    val parent = Some(Symbol("Parent"))
+
+    val exampleEdgeSub = MultipleValuesStandingQuery.SubscribeAcrossEdge(
+      edgeName = parent,
+      edgeDirection = Some(EdgeDirection.Outgoing),
+      andThen = MultipleValuesStandingQuery.UnitSq()
+    )
+
+    val result = Pretty.treePrint(exampleEdgeSub)
+
+    val expected =
+      s"""SubscribeAcrossEdge (
+        |\tedgeName = $parent,
+        |\tedgeDirection = Some(Outgoing),
+        |\tandThen =
+        |\t\tUnit
+        |)""".stripMargin
+
+    assert(result == expected)
+  }
+
+  "treePrint(exampleEdgeRecip)" should "generate expected pretty output" in {
+    val id = UUID.randomUUID()
+
+    val unknown = Symbol("Unknown")
+
+    val exampleEdgeRecip = MultipleValuesStandingQuery.EdgeSubscriptionReciprocal(
+      halfEdge = HalfEdge(unknown, EdgeDirection.Incoming, QuineId("hello, world".getBytes("ASCII"))),
+      andThenId = MultipleValuesStandingQueryPartId(id)
+    )
+
+    val result = Pretty.treePrint(exampleEdgeRecip)
+
+    val expected =
+      s"""EdgeSubscriptionReciprocal (
+         |\thalfEdge = HalfEdge($unknown,Incoming,QuineId(68656C6C6F2C20776F726C64)),
+         |\tandThenId = MultipleValuesStandingQueryPartId($id)
+         |)""".stripMargin
+
+    assert(result == expected)
+  }
+
+  "treePrint(exampleFilterMap)" should "generated expected pretty output" in {
+    val a = Symbol("a")
+
+    val exampleFilterMap = MultipleValuesStandingQuery.FilterMap(
+      condition = Some(Expr.Equal(Expr.Variable(a), Expr.Str("hello"))),
+      toFilter = MultipleValuesStandingQuery.UnitSq(),
+      dropExisting = true,
+      toAdd = Nil
+    )
+
+    val result = Pretty.treePrint(exampleFilterMap)
+
+    val expected =
+      s"""FilterMap (
+         |\tcondition = Some(Equal(Variable($a),Str(hello))),
+         |\ttoFilter =
+         |\t\tUnit,
+         |\tdropExisting = true,
+         |\ttoAdd = List()
+         |)""".stripMargin
+
+    assert(result == expected)
+  }
+}
