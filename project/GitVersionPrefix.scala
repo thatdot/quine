ad220fd3bd7a4691c1c38d5a5e20990be57e099f
Ethan Bell <eb@ethanbell.me>
2024-01-24 12:43:27 -0800

    Add documentation to GitVersionPrefix

--- project/GitVersionPrefix.scala
+++ project/GitVersionPrefix.scala
@@ -1,3 +1,7 @@
+// The exact structure of this object is important. Namely, this file should
+// contain an object called `GitVersionPrefix` with a single member `string`.
+// This keeps open source builds happily available for integration with the
+// greater thatDot ecosystem.
 object GitVersionPrefix {
   val string = "v"
 }
