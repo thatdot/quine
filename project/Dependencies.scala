cff929c78479564ef5f2ac43972befbdc50266ac
Ben Kyrlach <bkyrlach@gmail.com>
2024-03-20 14:48:46 -0400

    Fixes QU-1802
    * Updated to newer version of `quine-query-language` to avoid a transitive dependency on a library with a CVE
    * Updated a transitive dependency to align versions
    
    GitOrigin-RevId: 4b79d65c73caab7a81e0d1ac494ccb392b92559d

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -53,7 +53,7 @@ object Dependencies {
   val protobufV = "3.25.3"
   val protobufCommonV = "2.14.2"
   val pureconfigV = "0.17.6"
-  val quineQueryV = "0.1.1"
+  val quineQueryV = "0.1.3"
   val reactPlotlyV = "2.5.1"
   val reactV = "17.0.2"
   val rocksdbV = "8.11.3"
