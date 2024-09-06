b3214065fbe6c1fbcaae433aa4db1ba2c46217f9
Christopher Meade <2099307+cmeade@users.noreply.github.com>
2024-09-06 12:51:12 -0700

    Make endpoints wait for app load
    Enterprise:
    This is a first step towards improving the reliability and
    discoverability of our top level component structure. This causes the
    HTTP server in thatDot Streaming Graph to reject requests until app data
    has been loaded.
    
    QU-1762
    
    GitOrigin-RevId: 4f54362a8cf2954e2ed293ad041685cd3e03b5be

--- quine-core/src/main/scala/com/thatdot/quine/graph/GraphService.scala
+++ quine-core/src/main/scala/com/thatdot/quine/graph/GraphService.scala
@@ -132,6 +132,8 @@ class GraphService(
   @volatile private var _isReady = true
   def isReady: Boolean = _isReady
 
+  def isAppLoaded: Boolean = true
+
   override def shutdown(): Future[Unit] = {
     _isReady = false
     implicit val ec = nodeDispatcherEC
