7cc17acd089847d907d3166c3021f1fdc1fdc52d
Hakky54 <hakangoudberg@hotmail.com>
2025-09-11 09:46:17 -0700

    Bump and rename sslcontext-kicktart to ayza
    Bump and rename sslcontext-kicktart to ayza
    
    Closes thatdot/quine#54
    
    ---------
    
    Co-authored-by: Hakky54 <hakangoudberg@hotmail.com>
    Co-authored-by: Matt I.C. <26103520+InmanCochrane@users.noreply.github.com>
    GitOrigin-RevId: 928bc0fc86d805e2e9e09afdf2c6197a749251ee

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -73,7 +73,7 @@ object Dependencies {
   val scoptV = "4.1.0"
   val shapelessV = "2.3.13"
   val slinkyV = "0.7.4"
-  val sslContextKickstartV = "9.0.0"
+  val ayzaV = "10.0.0"
   val stoplightElementsV = "9.0.1"
   val sugarV = "2.0.6"
   val tapirV = "1.11.33"
