30b494009c880561bcecf508bb0508e92ca7bb71
Matt I.C <26103520+InmanCochrane@users.noreply.github.com>
2025-11-24 09:57:48 -0800

    Add StandingQueryResultWorkflow schema and deps
    Adds explicit `Schema` definitions for `StandingQueryResultWorkflow` and `QuineDestinationSteps` types in `V2StandingApiSchemas.scala`.
    
    GitOrigin-RevId: 1ea83ffe1d7462135e0a66039198f461715ab2c4

--- quine/src/main/scala/com/thatdot/quine/app/v2api/endpoints/V2StandingApiSchemas.scala
+++ quine/src/main/scala/com/thatdot/quine/app/v2api/endpoints/V2StandingApiSchemas.scala
@@ -2,13 +2,20 @@ package com.thatdot.quine.app.v2api.endpoints
 
 import cats.data.NonEmptyList
 import io.circe.{Decoder, Encoder}
+import sttp.tapir.Schema
 
+import com.thatdot.api.v2.outputs.OutputFormat
 import com.thatdot.api.v2.schema.V2ApiSchemas
+import com.thatdot.api.v2.{AwsCredentials, AwsRegion}
 import com.thatdot.quine.app.v2api.definitions.outputs.QuineDestinationSteps
 import com.thatdot.quine.app.v2api.definitions.query.standing.Predicate.OnlyPositiveMatch
 import com.thatdot.quine.app.v2api.definitions.query.standing.StandingQueryPattern.StandingQueryMode
 import com.thatdot.quine.app.v2api.definitions.query.standing.StandingQueryResultTransformation.InlineData
-import com.thatdot.quine.app.v2api.definitions.query.standing.StandingQueryResultWorkflow
+import com.thatdot.quine.app.v2api.definitions.query.standing.{
+  Predicate,
+  StandingQueryResultTransformation,
+  StandingQueryResultWorkflow,
+}
 
 trait V2StandingApiSchemas extends V2ApiSchemas {
 
@@ -36,5 +43,23 @@ trait V2StandingApiSchemas extends V2ApiSchemas {
   val exampleStandingQueryResultWorkflows: Seq[StandingQueryResultWorkflow] = Seq(
     exampleStandingQueryResultWorkflowToStandardOut,
   )
+
+  //////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+  // Explicit Schema definitions to avoid repeated automatic derivation (QU-2417)
+  // (This may not be the ideal long-term solution, but it's low-hanging fruit for faster compilation).
+  implicit lazy val predicateSchema: Schema[Predicate] = Schema.derived
+  implicit lazy val standingQueryResultTransformationSchema: Schema[StandingQueryResultTransformation] = Schema.derived
+  implicit lazy val outputFormatSchema: Schema[OutputFormat] = Schema.derived
+  // Note: AwsCredentials/AwsRegion also defined in V2IngestApiSchemas
+  implicit lazy val standingAwsCredentialsSchema: Schema[AwsCredentials] = Schema.derived
+  implicit lazy val standingAwsRegionSchema: Schema[AwsRegion] = Schema.derived
+  implicit lazy val destinationCypherQuerySchema: Schema[QuineDestinationSteps.CypherQuery] = Schema.derived
+  implicit lazy val quineDestinationStepsSchema: Schema[QuineDestinationSteps] = Schema.derived
+
+  // Consider adding tapir-cats in the future
+  implicit def nonEmptyListSchema[A](implicit inner: Schema[A]): Schema[NonEmptyList[A]] =
+    Schema.schemaForIterable[A, List].map(list => NonEmptyList.fromList(list))(_.toList)
+
+  implicit lazy val standingQueryResultWorkflowSchema: Schema[StandingQueryResultWorkflow] = Schema.derived
 
 }
