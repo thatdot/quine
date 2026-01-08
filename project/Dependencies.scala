722da2426db8172910f3af975740f55ffd1d96f9
Matt I.C <26103520+InmanCochrane@users.noreply.github.com>
2026-01-08 08:59:51 -0800

    Update msgpack from 0.9.10 to 0.9.11
    Update msgpack one patch version. CVE-2026-21452
    
    GitOrigin-RevId: facf357b494a13d22d5469d8457916b85643500f

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -41,7 +41,7 @@ object Dependencies {
   val lz4JavaV = "1.8.0" // Try to keep this in sync w/ the version kafka-client depends on.
   val mapDbV = "3.1.0"
   val metricsInfluxdbV = "1.1.0"
-  val msgPackV = "0.9.10"
+  val msgPackV = "0.9.11"
   val nettyOverrideV = "4.1.129.Final"
   val openApiCirceYamlV = "0.11.9"
   val openCypherV = "9.2.3"
